efd19ce5a2db0c75f6e236342293139277d23734
kannakamui <sam.niray@outlook.com>
2024-04-14 05:54:25 -0500

    creacion proyecto registro matriculas

---
+++ registro-matricula/src/main/java/com/miacademia/RegistroMatriculaApplication.java
@@ -0,0 +1,13 @@
+package com.miacademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroMatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroMatriculaApplication.class, args);
+	}
+
+}
---
+++ registro-matricula/src/test/java/com/miacademia/RegistroMatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miacademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroMatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
